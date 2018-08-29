f27ec11eaaddc08e5696cf641413e467de1a39b4
Evgeniy Dyachkov <dyachkov1997@yandex.ru>
2018-08-30 01:59:52 +0300

    Webpack and Babel setup

---
+++ config/webpack.dev.js
@@ -0,0 +1,59 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: ["./src/main.js"]
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].html"
+            }
+          },
+          "extract-loader",
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
---
+++ src/main.js
@@ -0,0 +1,12 @@
+require("@babel/runtime/helpers/asyncToGenerator");
+require("@babel/runtime/regenerator");
+require("./main.css");
+require("./index.html");
+
+var a = async args => {
+  const { a, b } = args;
+  await console.log("hello WWorld");
+  console.log("done");
+};
+
+a({ a: 1, b: 2 });
