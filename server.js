814df5158a633686795153e6a267c233a0c39d67
kokosin4ik <dyachkov.ea@phystech.edu>
2018-09-02 13:10:02 +0300

    Heroku preparations

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  console.log('server listening on port: ', port);
+});
