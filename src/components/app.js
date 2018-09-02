406e9791f9a205174031f35ffe6bdadadd8f95f9
kokosin4ik <dyachkov.ea@phystech.edu>
2018-09-02 18:44:54 +0300

    Refacroting

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-import './app.css'
+import React from "react";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import "./app.css";
 const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -14,35 +14,59 @@ export default class App extends React.Component {
             <Menu
               theme="dark"
               mode="horizontal"
-              defaultSelectedKeys={['2']}
-              style={{ lineHeight: '64px' }}
+              defaultSelectedKeys={["2"]}
+              style={{ lineHeight: "64px" }}
             >
               <Menu.Item key="1">nav 1</Menu.Item>
               <Menu.Item key="2">nav 2</Menu.Item>
               <Menu.Item key="3">nav 3</Menu.Item>
             </Menu>
           </Header>
-          <Layout style={{height: window.innerHeight - 64}}>
-            <Sider width={200} style={{ background: '#fff' }}>
+          <Layout style={{ height: window.innerHeight - 64 }}>
+            <Sider width={200} style={{ background: "#fff" }}>
               <Menu
                 mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0 }}
+                defaultSelectedKeys={["1"]}
+                defaultOpenKeys={["sub1"]}
+                style={{ height: "100%", borderRight: 0 }}
               >
-                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                <SubMenu
+                  key="sub1"
+                  title={
+                    <span>
+                      <Icon type="user" />
+                      subnav 1
+                    </span>
+                  }
+                >
                   <Menu.Item key="1">option1</Menu.Item>
                   <Menu.Item key="2">option2</Menu.Item>
                   <Menu.Item key="3">option3</Menu.Item>
                   <Menu.Item key="4">option4</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                <SubMenu
+                  key="sub2"
+                  title={
+                    <span>
+                      <Icon type="laptop" />
+                      subnav 2
+                    </span>
+                  }
+                >
                   <Menu.Item key="5">option5</Menu.Item>
                   <Menu.Item key="6">option6</Menu.Item>
                   <Menu.Item key="7">option7</Menu.Item>
                   <Menu.Item key="8">option8</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                <SubMenu
+                  key="sub3"
+                  title={
+                    <span>
+                      <Icon type="notification" />
+                      subnav 3
+                    </span>
+                  }
+                >
                   <Menu.Item key="9">option9</Menu.Item>
                   <Menu.Item key="10">option10</Menu.Item>
                   <Menu.Item key="11">option11</Menu.Item>
@@ -50,13 +74,20 @@ export default class App extends React.Component {
                 </SubMenu>
               </Menu>
             </Sider>
-            <Layout style={{ padding: '0 24px 24px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
+            <Layout style={{ padding: "0 24px 24px" }}>
+              <Breadcrumb style={{ margin: "16px 0" }}>
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
                 <Breadcrumb.Item>List</Breadcrumb.Item>
                 <Breadcrumb.Item>App</Breadcrumb.Item>
               </Breadcrumb>
-              <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+              <Content
+                style={{
+                  background: "#fff",
+                  padding: 24,
+                  margin: 0,
+                  minHeight: 280
+                }}
+              >
                 Content
               </Content>
             </Layout>
@@ -65,4 +96,4 @@ export default class App extends React.Component {
       </div>
     );
   }
-};
+}
