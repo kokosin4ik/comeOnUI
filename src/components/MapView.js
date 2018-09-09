ceac9cec27be6657009134eb1a86625ef097c74b
kokosin4ik <dyachkov.ea@phystech.edu>
2018-09-09 17:56:29 +0300

    Add leaflet marker and popUp

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -9,20 +9,22 @@ import './MapView.css';
 
 export default class MapView extends React.Component {
   
-  
   componentDidMount() {
     // create map
-    this.map = L.map('map', {
-      center: [49.8419, 24.0315],
-      zoom: 16,
-      layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }),
-      ]
-    });
+    this.mymap = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      // accessToken: 'your.mapbox.access.token'
+    }).addTo(this.mymap);
+    
+    var marker = L.marker([51.5, -0.09]).addTo(this.mymap);
+    marker.bindPopup("<b>Hello world!</b><br>You are here.").openPopup();
+    
   }
   render() {
+    
     return <div id="map"></div>
   }
 }
