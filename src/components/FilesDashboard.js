4662bb9cd79cee6ffb7ee3496630fd6ed7d00d6c
Pavel Kvasha <pashakvasha@yandex.ru>
2018-12-08 11:48:14 +0300

    add file error handling

--- src/components/FilesDashboard.js
+++ src/components/FilesDashboard.js
@@ -12,6 +12,7 @@ class FilesDashboard extends React.Component {
         this.state = {
             fileList: [],
             message: '',
+            errorMessage: ''
         }
 
         this.uploadFiles = this.uploadFiles.bind(this);
@@ -30,12 +31,16 @@ class FilesDashboard extends React.Component {
             .then(handleResponse)
             .then((file) => {
                 this.setState({
-                    message: `${file.name} has been successfully decrypted`
+                    message: `${file.name} has been successfully decrypted`,
+                    errorMessage: ''
                 });
                 this.fetchFiles();
             })
             .catch((error) => {
-                console.log("Error");
+                this.setState({
+                    message: '',
+                    errorMessage: error.message
+                });
             });
     }
 
@@ -46,12 +51,16 @@ class FilesDashboard extends React.Component {
             .then(handleResponse)
             .then((file) => {
                 this.setState({
-                    message: `${file.name} has been successfully encrypted`
+                    message: `${file.name} has been successfully encrypted`,
+                    errorMessage: ''
                 });
                 this.fetchFiles();
             })
             .catch((error) => {
-                console.log("Error");
+                this.setState({
+                    message: '',
+                    errorMessage: error.message
+                });
             });
     }
 
@@ -75,12 +84,16 @@ class FilesDashboard extends React.Component {
             .then(handleResponse)
             .then((file) => {
                 this.setState({
-                    message: `${file.name} has been successfully added`
+                    message: `${file.name} has been successfully added`,
+                    errorMessage: ''
                 });
                 this.fetchFiles();
             })
             .catch((error) => {
-                console.log("Error");
+                this.setState({
+                    message: '',
+                    errorMessage: error.message
+                });
             });
     }
 
@@ -94,6 +107,12 @@ class FilesDashboard extends React.Component {
                     {
                         this.state.message &&
                         <div style={{margin: '0 0 12 32', color: '#0b8235'}}><i>{this.state.message}</i></div>
+                    }
+                    {
+                        this.state.errorMessage &&
+                        <div style={{color: 'f5222d', margin: '0 0 12 32'}}>
+                            <i>{this.state.errorMessage}</i>
+                        </div>
                     }
                     <FileTree
                         fileList={this.state.fileList}
